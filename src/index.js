eb1c6212f684053bb20e98800ed5f3bfbee1df1b
Ctouchdev <63693926+Ctouchdev@users.noreply.github.com>
2020-10-14 11:08:40 -0500

    Updates

--- src/index.js
+++ src/index.js
@@ -1,11 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import PI, { doublePi, triplePi } from "./math";
 
 ReactDOM.render(
   <ul>
-    <li>1</li>
-    <li>2</li>
-    <li>3</li>
+    <li>{PI}</li>
+    <li>{doublePi()}</li>
+    <li>{triplePi()}</li>
   </ul>,
   document.getElementById("root")
 );
+
+//Can also import "ALL"
+
+// import * as pi from "./math";
+//   <ul>
+//     <li>{pi.default}</li>
+//     <li>{pi.doublePi()}</li>
+//     <li>{pi.triplePi()}</li>
+//   </ul>
